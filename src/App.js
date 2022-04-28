44ebcb530d4a4682ed3c30cb4bc729eed0686880
Mbulelo Pani <mbulelopani09@gmail.com>
2022-04-28 22:03:25 +0200

    testing for deploy

--- src/App.js
+++ src/App.js
@@ -1,34 +1,26 @@
 import {useState, useEffect} from 'react';
-// import Dashboard from './Components/Dashboard';
-import axios from 'axios';
+import Dashboard from './Components/Dashboard';
+
 function App() {
-  // const [permission, setPermission] = useState(false);
+  const [permission, setPermission] = useState(false);
   const [coords, setCoords] = useState({});
   useEffect(() => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((res) => {
-        // setPermission(true);
         setCoords({
           latitude: res.coords.latitude,
           longitude: res.coords.longitude
         });
-        
-        axios.get("http://api.weatherapi.com/v1/forecast.json?key=a725b42ab3ce4d768bb15630222304&q=" + coords.latitude + "," + coords.longitude + "&days=2")
-        .then((res) => {            
-            // setData(res)
-            console.log("rrr", res.data)
-        })
-        .catch((err) => console.log("error: ",err))
+        setPermission(true);
       });
     } else { 
-      // setPermission(false)
+      setPermission(false)
     }
-  })
+
+  }, [])
   return (
     <div className="App">
-        {/* <Dashboard coords={coords} permission={permission}/> :  */}
-        
-        <h1>TEST</h1>
+        <Dashboard coords={coords} permission={permission} /> 
     </div>
   );
 }
--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,8 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import moment from 'moment';
-import icon from '../Images/icon.jpg'
-const Dashboard = ({coords, permission}) => {
+import icon from '../Images/icon.jpg';
+import Chart from './Chart';
+
+const Dashboard = ({coords}) => {
     const {latitude, longitude} = coords;
     const [place, setPlace] = useState('')
     const [forecast, setForecast] = useState({});
@@ -27,23 +29,20 @@ const Dashboard = ({coords, permission}) => {
     });
     const {name, country, localtime} = location;
 
-    
-    useEffect((permission) => {
-        
+    useEffect( (permission) => {
         if(permission === true){
-            getWeather(); 
+            axios.get("http://api.weatherapi.com/v1/forecast.json?key=a725b42ab3ce4d768bb15630222304&q=" + latitude + "," + longitude + "&days=2")
+            .then((res) => setData(res))
+            .catch(err => console.log(err))
+            
         }
-    });
+    }, [latitude,longitude]);
 
-    const getWeather = (latitude, longitude) => {
+    const getWeather =  (latitude, longitude) => {
         axios.get("http://api.weatherapi.com/v1/forecast.json?key=a725b42ab3ce4d768bb15630222304&q=" + latitude + "," + longitude + "&days=2")
-        .then((res) => {            
-            setData(res)
-            console.log("rrr", res.data)
-        })
-        .catch((err) => console.log("error: ",err))
+        .then((res) => setData(res))
+        .catch(err => console.log(err))
     }
-    
     if(place.length > 0) {
         axios.get("http://api.weatherapi.com/v1/forecast.json?key=a725b42ab3ce4d768bb15630222304&q=" + place + "&days=2")
         .then((res) => {            
@@ -67,53 +66,54 @@ const Dashboard = ({coords, permission}) => {
             temp_c, last_updated, current_condition
         })
         setForecast(res.data.forecast.forecastday)
-        console.log("xx",res.data)
     }
 
-
-    const searchWeather = (place) => {
-        axios.get("http://api.weatherapi.com/v1/forecast.json?key=a725b42ab3ce4d768bb15630222304&q=" + place + "&days=2")
-        .then((res) => {            
-            setData(res)
-        })
-        .catch((err) => console.log("error: ",err));
-    }
     return (
         <div className="container" >
             {forecast.length > 0 ? 
                 <div>                
                     <div className="card" style={{textAlign: 'center', marginTop:'2em'}}>
                         <div>
-                            <input type="button" class="w3-button w3-orange inline-border text-white" value={place} onClick={searchWeather}/>aa
-                            <input type="text" placeholder="Search city" className="w3-input" value={place} onChange={(e) => setPlace(e.target.value)} style={{display: 'inline-block',paddingLeft: '1em', paddingTop:'0.4em',width:'30%',marginBottom:'1em', marginLeft:'.5em', backgroundColor: '#F1F3F4',}}/>
+                            {/* <input type="button" class="w3-button w3-orange inline-border text-white" value={place} onClick={searchWeather} placeholder="Search city"/> */}
+                            <input type="text" placeholder="Search city" className="w3-input" value={place} onChange={(e) => setPlace(e.target.value)} style={{display: 'inline-block',paddingLeft: '1em', paddingTop:'0.4em',width:'30%',marginBottom:'1em', marginLeft:'.5em', backgroundColor: '#F1F3F4', borderRadius:'50px'}}/>
                         </div>
                         <h4>{moment(localtime).format('dddd, h:mma')}</h4>    
                         <h3 style={{fontSize:'80px'}}>
                             {name}, {country.indexOf(' ') > 0  ? country.match(/(\b\S)?/g).join("").toUpperCase().slice(0, 3) : country.slice(0, 3)}
                         </h3>
-                        <p style={{fontSize:'55px', textAlign:'center', marginTop:'-0.5em'}}>
+                        <p style={{fontSize:'55px', textAlign:'center', marginTop:'-0.5em', color:'orange'}}>
                             <img src={forecast.length > 1 && forecast[0].day.condition.icon} alt="Avatar" className="" style={{width:"10%"}} />
                             {temp_c}°C
                         </p>
                     </div>
-                    <div style={userStyle}>
+                    <div >
                         { forecast.length > 0 && forecast.map((f) => (
-                            <div style={{textAlign: 'center'}}>
+                            <>
+                            <div style={{textAlign: 'center', backgroundColor: 'black', opacity:'0.5'}}>
                                 <h4>{moment(f.date).format('dddd')}</h4>
-                                <h1>max {f.day.maxtemp_c}°C</h1>
-                                <h6>
-                                    Rain: {f.day.daily_chance_of_rain}%
-                                </h6>
-                                <h6>
-                                    Wind: {f.day.maxwind_kph} kph
-                                    {/* {f.day.condition.text} */}
-                                </h6>
+                                max<h1 style={{display: 'inline-block', color:'orange'}}> 
+                                    {f.day.maxtemp_c}°C
+                                </h1>
+                                <div>
+                                    Rain: <h4 style={{display: 'inline-block', color:'orange'}}>
+                                        {f.day.daily_chance_of_rain}%
+                                    </h4>
+                                </div>
+                                <div>
+                                    Wind: <h4 style={{display: 'inline-block', color:'orange'}}>
+                                        {f.day.maxwind_kph} kph
+                                    </h4>
+                                </div>
                             </div>
+                            <div>
+                                <Chart coords={coords}/>
+                            </div>
+                            </>
+                            
                         ))}
                     </div> 
                 </div> :
                 <div>
-                    <div>                
                     <div className="card" style={{textAlign: 'center', marginTop:'2em'}}>
                         <div>
                             <input type="text" placeholder="Search city" className="w3-input" value={place} onChange={(e) => setPlace(e.target.value)} style={{display: 'inline-block',paddingLeft: '1em', paddingTop:'0.4em',width:'30%',marginBottom:'1em', marginLeft:'.5em', backgroundColor: '#F1F3F4',}}/>
@@ -126,23 +126,21 @@ const Dashboard = ({coords, permission}) => {
                             {temp_c}°C
                         </p>
                     </div>
-                    
-                </div>
                 </div>
             }
-                
         </div>
-        
-        
     )
 }
 
 const userStyle = {
     width: '50%',
+    backgroundColor: 'black',
+    opacity: '0.6',
     display: 'grid',
     gridTemplateColumns: 'repeat(2, 1fr)',
     gridColumnGap: '1em',
     marginTop: '5em', 
-    margin: 'auto'
+    margin: 'auto',
 }
-export default Dashboard
+
+export default Dashboard;
